637d89d62304adab27f2c1d4bc26a4f7cffa77ec
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-06-30 12:59:18 +0800

    Quotes in the opposite direction.

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import cpp_lib
 import json
 
-print(f"1+2: {cpp_lib.add(1, 2)}")
+print(f'1+2: {cpp_lib.add(1, 2)}')
 
-print(f"DUP: {cpp_lib.duplicate('foo')}")
+print(f'DUP: {cpp_lib.duplicate("foo")}')
 
-print(f"ADD: {cpp_lib.add_from_json(json.dumps({'a':1,'b': 2}))}")
-print(f"ERR: {cpp_lib.add_from_json(json.dumps({'foo':'bar'}))}")
+print(f'ADD: {cpp_lib.add_from_json(json.dumps({"a":1,"b":2}))}')
+print(f'ERR: {cpp_lib.add_from_json(json.dumps({"foo":"bar"}))}')
 
 s0 = cpp_lib.s1m()
-print(f"S1M[-5:]: {s0[-5:]}, len={len(s0)}")
+print(f'S1M[-5:]: {s0[-5:]}, len={len(s0)}')
 
 for i in range(0, 5):
   s1 = cpp_lib.growing_s()
-  print(f"SPP[-5:]: {s1[-5:]}, len={len(s1)}")
+  print(f'SPP[-5:]: {s1[-5:]}, len={len(s1)}')
